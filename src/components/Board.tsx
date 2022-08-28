522d755257d5de53ddeb4de2805262fcc42ecd8e
“josh-cook” <cookiej91@gmail.com>
2022-08-28 14:03:33 +0100

    Add optimisation

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,28 @@
 import { Circle, Flex } from "@chakra-ui/react";
 import { boardRows, playerColor } from "const";
-import { usePlayPiece } from "hooks";
+import { usePlayPieceWithBot } from "hooks";
 import { FC } from "react";
 import { useRecoilValue } from "recoil";
 import { boardState, gameOverState, playerState } from "state";
 import { Player } from "types";
 
-const padCol = (col: number[]): number[] =>
-  col.join("").padEnd(boardRows, "0").split("").map(Number);
+/**
+ * https://jsben.ch/K2bj0 - 40% faster than original (commented below)
+ * const padCol = (col: number[]): number[] =>
+ *   col.join("").padEnd(boardRows, "0").split("").map(Number);
+ */
+const padColOptimisation = (col: number[]) => {
+  let columnCopy = col.slice();
+
+  while (columnCopy.length < boardRows) {
+    columnCopy.push(0);
+  }
+
+  return columnCopy;
+};
 
 const Board: FC = () => {
-  const play = usePlayPiece();
+  const play = usePlayPieceWithBot();
   const board = useRecoilValue(boardState);
   const player = useRecoilValue(playerState);
   const gameOver = useRecoilValue(gameOverState);
@@ -25,7 +37,7 @@ const Board: FC = () => {
           flexDirection="column-reverse"
           cursor={gameOver ? "auto" : "pointer"}
         >
-          {padCol(col).map((p, j) => (
+          {padColOptimisation(col).map((p, j) => (
             <Circle
               m={1}
               size="40px"

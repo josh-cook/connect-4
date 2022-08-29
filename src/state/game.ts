b24f9b7fec5eb76dcc169aa8262b863573d955a2
“josh-cook” <cookiej91@gmail.com>
2022-08-29 09:25:09 +0100

    Add state persistence

--- src/state/game.ts
+++ src/state/game.ts
@@ -1,23 +1,28 @@
 import { boardCols } from "const";
 import { atom } from "recoil";
 import { Board, Player } from "types";
+import localStorageEffect from "./storage";
 
 export const boardState = atom<Board>({
   key: "boardState",
   default: Array(boardCols).fill([]),
+  effects: [localStorageEffect("boardState")],
 });
 
 export const playerState = atom<Player>({
   key: "playerState",
   default: 1,
+  effects: [localStorageEffect("playerState")],
 });
 
 export const gameOverState = atom<boolean>({
   key: "gameOverState",
   default: false,
+  effects: [localStorageEffect("gameOverState")],
 });
 
 export const botState = atom<boolean>({
   key: "botState",
   default: false,
+  effects: [localStorageEffect("botState")],
 });
